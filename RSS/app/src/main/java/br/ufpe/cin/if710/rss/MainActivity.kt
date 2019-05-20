56a9242c2d851ddbf5a89b1b49639f1536c01474
joaoadherval <joaoadherval@gmail.com>
2019-05-20 10:51:59 -0300

    usando a classe ParserRSS para processar o arquivo XML

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
@@ -14,8 +14,7 @@ class MainActivity : Activity() {
     //private val RSS_FEED = "http://pox.globo.com/rss/g1/ciencia-e-saude/"
     //private val RSS_FEED = "http://pox.globo.com/rss/g1/tecnologia/"
 
-    //trocar para listview depois
-    private TextView conteudoRSS;
+    private val conteudoRSS: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,37 +25,51 @@ class MainActivity : Activity() {
     protected fun onStart() {
         super.onStart()
         try {
-            //mudar código para lidar com acesso a internet fora da thread principal
-            val feedXML = getRssFeed(RSS_FEED)
-            conteudoRSS.setText(feedXML)
+            var textXML = CustomAsyncTask().execute(RSS_FEED)
+            var listRSS = ParserRSS.parse(textXML)
+            //conteudoRSS.setText(textXML)
         } catch (e: IOException) {
             e.printStackTrace()
         }
 
     }
 
-    //Opcional - pesquise outros meios de obter arquivos da internet - bibliotecas, etc.
-    @Throws(IOException::class)
-    private fun getRssFeed(feed: String): String {
-        var input: InputStream? = null
-        var rssFeed = ""
-        try {
-            val url = URL(feed)
-            val conn = url.openConnection() as HttpURLConnection
-            input = conn.getInputStream()
-            val out = ByteArrayOutputStream()
-            val buffer = ByteArray(1024)
-            var count: Int
-            while ((count = input!!.read(buffer)) != -1) {
-                out.write(buffer, 0, count)
-            }
-            val response = out.toByteArray()
-            rssFeed = String(response, "UTF-8")
-        } finally {
-            if (input != null) {
-                input!!.close()
-            }
+    inner class CustomAsyncTask: AsyncTask<String, Void, String>() {
+        override fun doInBackgroud(vararg params: String?): String {
+            return result = getRssFeed(params[0])
         }
-        return rssFeed
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        //Opcional - pesquise outros meios de obter arquivos da internet - bibliotecas, etc.
+        @Throws(IOException::class)
+        private fun getRssFeed(feed: String): String {
+            var input: InputStream? = null
+            var rssFeed = ""
+            try {
+                val url = URL(feed)
+                val conn = url.openConnection() as HttpURLConnection
+                input = conn.getInputStream()
+                val out = ByteArrayOutputStream()
+                val buffer = ByteArray(1024)
+                var count: Int
+                while ((count = input!!.read(buffer)) != -1) {
+                    out.write(buffer, 0, count)
+                }
+                val response = out.toByteArray()
+                rssFeed = String(response, "UTF-8")
+            } finally {
+                if (input != null) {
+                    input!!.close()
+                }
+            }
+            return rssFeed
+        }
     }
 }
